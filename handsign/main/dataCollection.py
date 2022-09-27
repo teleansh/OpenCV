0776757abb92215e9f213295919dddb12089549e
Hopeless Coder <34571056+teleansh@users.noreply.github.com>
2022-09-27 22:05:23 +0530

    Hand Sign/Posture recognition

---
+++ handsign/main/dataCollection.py
@@ -0,0 +1,53 @@
+import cv2 as cv
+from cvzone.HandTrackingModule import HandDetector
+import numpy as np
+import math
+import time
+
+cap = cv.VideoCapture(0)
+detector = HandDetector(maxHands=1)
+
+offset = 20
+imgSize = 300
+
+# This file is to train the model
+
+folder = "data/spark"   #Folder location which is to be trained
+counter = 0             #Counts number of images stored
+
+while True:
+    success, img = cap.read()
+    hands, img = detector.findHands(img)
+    if hands:
+        hand = hands[0]
+        x, y, w, h = hand['bbox']
+
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
+        imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
+        imgCropShape = imgCrop.shape
+        ar = h / w
+        if ar > 1:
+            k = imgSize / h
+            wCal = math.ceil(k * w)
+            imgResize = cv.resize(imgCrop, (wCal, imgSize))
+            imgResizeShape = imgResize.shape
+            wGap = math.ceil((imgSize - wCal) / 2)
+            imgWhite[:, wGap:wCal + wGap] = imgResize
+
+        else:
+            k = imgSize / w
+            hCal = math.ceil(k * h)
+            imgResize = cv.resize(imgCrop, (imgSize, hCal))
+            imgResizeShape = imgResize.shape
+            hGap = math.ceil((imgSize - hCal) / 2)
+            imgWhite[hGap:hCal + hGap, :] = imgResize
+
+        cv.imshow("Cropped", imgCrop)
+        cv.imshow("White", imgWhite)
+
+    cv.imshow("Result", img)
+    key = cv.waitKey(1)
+    if key == ord("s"):
+        counter += 1
+        cv.imwrite(f'{folder}/Image_{time.time()}.jpg',imgWhite)
+        print(counter)
